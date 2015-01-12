9bc64122353d5e98eb914c1aa13bbe21e0d372e4
luin <i@zihua.li>
2015-01-12 18:53:36 +0800

    Add controller to create project

--- routes/projects/index.js
+++ routes/projects/index.js
@@ -3,3 +3,11 @@ var router = module.exports = new (require('koa-router'))();
 router.get('/:projectId/collections/:collectionId', function *() {
   yield this.render('index');
 });
+
+router.post('/', function *() {
+  yield this.api.projects.post({
+    name: this.request.body.name
+  });
+
+  this.redirect('/');
+});
