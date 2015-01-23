47dbffedbfd5a34a583d48a113f8fd1e1504b313
luin <i@zihua.li>
2015-01-23 14:22:01 +0800

    Implement creating collection

--- routes/projects/index.js
+++ routes/projects/index.js
@@ -9,6 +9,15 @@ router.get('/:projectId/collections/:collectionId', function *() {
   yield this.render('index');
 });
 
+router.post('/:projectId/collections', function *() {
+  var collection = yield this.api.projects(this.params.projectId).collections.post({
+    name: this.request.body.name,
+    description: this.request.body.description
+  });
+  this.flash = { msgs: 'Create successfully.' };
+  this.redirect('/projects/' + this.params.projectId);
+});
+
 router.post('/', function *() {
   yield this.api.projects.post({
     name: this.request.body.name
@@ -16,3 +25,4 @@ router.post('/', function *() {
 
   this.redirect('/');
 });
+
