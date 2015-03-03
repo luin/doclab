8506811f62f2443ce7474a9387f33008d8201307
luin <i@zihua.li>
2015-03-03 10:51:22 +0800

    Support save doc

--- web/client/scripts/editor.js
+++ web/client/scripts/editor.js
@@ -1,12 +1,32 @@
 var Simditor = require('../editor/scripts/simditor');
 
 var editor = new Simditor({
-  textarea: $('.js-editor'),
+  textarea: $('.js-doc-editor'),
   toolbar: ['title', 'bold', 'italic', 'underline', 'strikethrough', 'color', 'ol', 'ul', 'blockquote', 'code', 'table', 'link', 'image', 'hr', 'indent', 'outdent'],
   tabIndent: false
 });
 
 require('./editor/layout');
+
+$('.js-doc-form').submit(function(event) {
+  event.preventDefault();
+
+  var $this = $(this);
+  var action = $this.attr('action');
+  var title = $('input[name="title"]').val() || 'Untitled';
+  var body = editor.getValue();
+
+  if (doclab.UUID) {
+  } else {
+    console.log(body);
+    $.api.post(action, {
+      title: title,
+      content: body
+    }, function(data) {
+      console.log(data);
+    });
+  }
+});
 
 if (doclab.UUID) {
   require('./editor/sync')(editor, doclab.UUID, (doclab.ssl ? 'wss://' : 'ws://') + doclab.host);
