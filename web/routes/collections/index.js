e95f7852dad37bfdc57a82ac6720262253685632
luin <i@zihua.li>
2015-03-03 10:52:14 +0800

    Doc list page

--- web/routes/collections/index.js
+++ web/routes/collections/index.js
@@ -3,11 +3,15 @@ var middlewares = require('../../middlewares');
 var config = require('config');
 var ot = require('ot-server').createClient(config.redis);
 
+router.param('collectionId', function *(id, next) {
+  this.locals.collection = yield this.api.collections(id).get();
+  this.locals.dirs = yield this.api.collections(id).dirs.get();
+  yield next;
+});
+
 router.get('/:collectionId', function *() {
-  this.locals.collection = yield this.api.collections(this.params.collectionId).get();
-  var dirs = yield this.api.collections(this.params.collectionId).dirs.get();
-  if (dirs.length > 0) {
-    this.redirect('/collections/' + this.params.collectionId + '/docs/' + dirs[0].UUID);
+  if (this.locals.dirs.length > 0) {
+    this.redirect('/collections/' + this.params.collectionId + '/docs/' + this.locals.dirs[0].UUID);
   } else {
     yield this.render('/collections/empty');
   }
@@ -17,8 +21,18 @@ router.get('/:collectionId/docs/new', middlewares.me(), function *() {
   yield this.render('/collections/editor');
 });
 
+router.get('/:collectionId/docs/:docUUID', function *() {
+  this.locals.doc = yield this.api.docs(this.params.docUUID).get();
+  console.log(this.locals.doc);
+  yield this.render('/collections/show');
+});
+
 router.post('/:collectionId/_move', function *() {
   this.body = yield this.api.collections(this.params.collectionId)._move.post({
     order: parseInt(this.request.body.order, 10)
   });
+});
+
+router.post('/:collectionId/docs', function *() {
+  this.body = yield this.api.collections(this.params.collectionId).docs.post(this.request.body);
 });
